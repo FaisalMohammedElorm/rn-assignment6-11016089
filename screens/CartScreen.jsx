00f109a0391ab7a020fe36ba2bcd0bcbdc60ed28
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-06-28 23:47:47 +0100

    screens

---
+++ screens/CartScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text , StyleSheet} from 'react-native'
+import React from 'react'
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text>CartScreen</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  
+});
+export default CartScreen
